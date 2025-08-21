3e4608aea83fb835ed549d211cf47cb9ec2051e5
Mayara Froes <froeselias721@gmail.com>
2025-08-21 11:28:06 -0400

    Alterações das variavéis no printf

--- Variáveis e Memoria RAM/variaveis-e-memoria.c
+++ Variáveis e Memoria RAM/variaveis-e-memoria.c
@@ -7,18 +7,18 @@ int main() {
     int a = 10;
     int b, c;
 
-    printf("&a = %p, a = %d\n", &a); // Endereço de memória da variável 'a', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
-    printf("&b = %p, b = %d\n", &a); // Endereço de memória da variável 'b', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
-    printf("&c = %p, c = %d\n\n", &a); // Endereço de memória da variável 'c', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
+    printf("&a = %p, a = %d\n", &a, a); // Endereço de memória da variável 'a', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
+    printf("&b = %p, b = %d\n", &b, b); // Endereço de memória da variável 'b', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
+    printf("&c = %p, c = %d\n\n", &c, c); // Endereço de memória da variável 'c', utilizamos a flag %p para imprimir o endereço, e %d para imprimir o valor
 
     b = 20;
-    c= a + b;
+    c = a + b;
 
 
     // fiz a atribuição de novos valores para b e c e imprimi novamente os valores e endereços de memória
-    printf("&a = %p, a = %d\n", &a); 
-    printf("&b = %p, b = %d\n", &a); 
-    printf("&c = %p, c = %d\n\n", &a);
+    printf("&a = %p, a = %d\n", &a, a); 
+    printf("&b = %p, b = %d\n", &b, b); 
+    printf("&c = %p, c = %d\n\n", &c, c);
 
     return 0;
 }
